98c7f8915b33c9792aa766b6686713624d0548dc
Mona <53706484+monafarrokhnejad@users.noreply.github.com>
2024-08-17 20:40:46 +0330

    add tailwind

--- app/page.js
+++ app/page.js
@@ -1,14 +1,16 @@
 import Link from "next/link";
 
 export default function Home() {
-    return (
-        <main>
-            <div className="container">
-                <h1>اپلیکیشن Quiz</h1>
-                <Link href="/quiz">
-                    <button>شروع آزمون</button>
-                </Link>
-            </div>
-        </main>
-    );
+  return (
+    <main>
+      <div className="container">
+        <h1 text-3xl font-bold underline>
+          اپلیکیشن Quiz
+        </h1>
+        <Link href="/quiz">
+          <button>شروع آزمون</button>
+        </Link>
+      </div>
+    </main>
+  );
 }
